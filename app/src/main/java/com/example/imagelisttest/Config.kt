d6329e56cbf361f131d232ed88f4f7e6ab69b1ec
Ry <pandora369741@yahoo.co.jp>
2021-05-14 00:00:31 +0900

    :+1: test API and http request

---
+++ app/src/main/java/com/example/imagelisttest/Config.kt
@@ -0,0 +1,5 @@
+package com.example.imagelisttest
+
+object Config {
+    const val API_KEY: String = "Mpm2euw4tMTyIWNVtdvLejuxn3IWaUV4G9j2NEu9ooA"
+}
--- app/src/main/java/com/example/imagelisttest/MainActivity.kt
+++ app/src/main/java/com/example/imagelisttest/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.imagelisttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.imagelisttest.bean.PhotoListBean
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mRequestQueue: RequestQueue
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mRequestQueue = Volley.newRequestQueue(this)
+        getImageData()
+    }
+
+    private fun getImageData(){
+        val url = "https://api.unsplash.com/photos/?page=1&per_page=10&order_by=popular"
+        var header = mutableMapOf("Authorization" to "Client-ID " + Config.API_KEY)
+
+        val jsonObjectRequest = GsonRequest<PhotoListBean>(url,PhotoListBean::class.java,header,createMyReqSuccessListener(),createMyReqErrorListener())
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+
+    private fun createMyReqSuccessListener(): Response.Listener<PhotoListBean>{
+        return Response.Listener {
+            print(it)
+        }
+    }
+
+
+    private fun createMyReqErrorListener(): Response.ErrorListener{
+        return Response.ErrorListener {
+            print(it)
+        }
     }
 }
